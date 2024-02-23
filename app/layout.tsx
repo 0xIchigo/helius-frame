da17a85b531e9747d3e7c302bf5f68b403783262
Evan <0xIchigo@protonmail.com>
2024-02-23 16:06:28 -0500

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
